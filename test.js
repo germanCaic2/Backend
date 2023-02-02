86d16322a337b0acc03cc9536ea5ced86ce6bea7
German Caicedo <german.caicedoa1@gmail.com>
2023-02-01 22:32:30 -0500

    se agrego deleteProducts

--- test.js
+++ test.js
@@ -7,6 +7,18 @@ const test = async () => {
   productManager.addProduct("nike", "sb", 2900, "nike-sb.png", 20);
   productManager.addProduct("vans", "classic", 2500, "vans-classic.png", 23);
   productManager.getProductById(1);
+  
+  // ObjetoModificado para actualizar
+  const ObjUpdated = {
+    title: "modicado",
+    description: "modicado",
+    price: "modicado",
+    thumbnail: "modicado",
+    stock: "modicado"
+  }
+  productManager.updateProduct(1, ObjUpdated)
+
+  productManager.deleteProduct(2)
 }
 
 test()
