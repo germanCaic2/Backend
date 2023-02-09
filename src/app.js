5005cea8defa72bca070d60d12bc6bf04dc0dad0
German Caicedo <german.caicedoa1@gmail.com>
2023-02-09 14:32:46 -0500

    "importacion de product manager"

--- src/app.js
+++ src/app.js
@@ -1,20 +1,28 @@
 const express = require('express');
-const ProductManager = require('./ProductManager');
-let productManager = new ProductManager();
 const app = express();
+const ProductManager = require('./ProductManager');
+const productManager = new ProductManager;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
-const products = require("../database/products.json");
-app.get("/products", (request, response) => { response.send(products) })
+// const getProducts = async () =>{
+//   products = await productManager.getProducts();
+//   return products
+// }
+
+app.get("/products", (request, response) => {
+  const getProducts = async () => products = await productManager.getProducts();
+  getProducts()
+  response.send(products)
+});
 
 app.get("/products/:id", (request, response) => {
-  const prod = products.find(p => p.id == request.params.id)
+  const prod = products.find(p => p.id == request.params.id);
   if (prod) { response.send(prod) } response.send({ message: "User not found" });
-})
+});
 
 const SERVER_PORT = 8080;
 app.listen(SERVER_PORT, () => {
-  console.log(`Server running in port ${SERVER_PORT}`)
+  console.log(`Server running in port ${SERVER_PORT}`);
 });
