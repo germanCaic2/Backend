5acbe1d18af543f7806faed199f516055056e06f
German Caicedo <german.caicedoa1@gmail.com>
2023-02-09 16:59:28 -0500

    "optimizacion general"

--- src/app.js
+++ src/app.js
@@ -7,23 +7,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 const getProducts = async () => products = await productManager.getProducts();
-getProducts()
+getProducts();
 
-app.get("/products", (request, response) => {
-  response.send(products)
-});
+app.get("/products", (request, response) => { response.send(products); });
 
 app.get("/products/:pid", (request, response) => {
   const product = products.find(p => p.id == request.params.pid);
   product ? response.send(product) : response.send({ message: "Product not found" });
 });
 
-app.get("/product/query", (request, response) => {
+app.get("/product/:query", (request, response) => {
   let limit = request.query.limit;
-  limit ? response.send(products.filter(p => p.id <= limit - 1)) : response.send({ message: "Product not found" });
+  limit ? response.send(products.filter(p => p.id <= limit - 1)) : response.send(products);
 });
 
 const SERVER_PORT = 8080;
-app.listen(SERVER_PORT, () => {
-  console.log(`Server running in port ${SERVER_PORT}`);
-});
+app.listen(SERVER_PORT, () => { console.log(`Server running in port ${SERVER_PORT}`); });
