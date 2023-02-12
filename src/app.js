e1afa18b7f189c6b46c3f8a3440a53b0710090c1
German Caicedo <german.caicedoa1@gmail.com>
2023-02-12 18:00:29 -0500

    ProductsQuery limit was fixed

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express  from "express";
+import express, { json } from "express";
 import ProductManager from "./ProductManager.js"
 
 const app = express();
@@ -13,17 +13,24 @@ let getProductByID = [];
 const getProducts = async () => products = await productManager.getProducts();
 getProducts();
 
-app.get("/products", (request, response) => {response.send(products);});
+app.get("/products", (request, response) => { response.send(products); });
 
 app.get("/products/:pid", (request, response) => {
-  const getProductsByID = async (id) => getProductByID = await productManager.getProductById(id);
-  getProductsByID(parseInt(request.params.pid))
-  response.send(getProductByID);
+  let id = parseInt(request.params.pid)
+  if (id <= products.length) {
+    const getProductsByID = async (id) => getProductByID = await productManager.getProductById(id);
+    getProductsByID(id)
+    response.send(getProductByID);
+  } else { response.send(products)}
 });
 
 app.get("/products:query", (request, response) => {
-  let limit = request.query.limit;
-  console.log(limit)
+  let productLimit = []
+  let limit = (parseInt(request.query.limit) - 1);
+  if (limit <= products.length) {
+    for (let i = 0; i <= limit; i++) { productLimit.push(products[i]) }
+    response.send(productLimit)
+  } else { response.send(products) }
 });
 
 const SERVER_PORT = 8080;
