53005f416aee4edb56241c7c53f9b00b92b958bf
German Caicedo <german.caicedoa1@gmail.com>
2023-02-16 12:24:11 -0500

    Product DELETE was added

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -18,7 +18,7 @@ router.get(`/:pid`, async (req, res) => {
   getProductsByID(id);
   id <= products.length ? res.send(getProductByID) : res.send(products);
 });
-
+// Recibe un JSON con el nuevo producto
 router.post(`/`, async (req, res) => {
   const addProduct = async (title, description, price, status, stock, category, thumbnail) => {
     await productManager.addProduct(title, description, price, status, stock, category, thumbnail);
@@ -27,12 +27,25 @@ router.post(`/`, async (req, res) => {
   addProduct(title, description, price, status, stock, category, thumbnail);
   res.send('ok');
 });
-
+// Recibe un JSON con los nuevos datos 
 router.put(`/:pid`, async (req, res) => {
   const id = (parseInt(req.params.pid))
   const newProduct = req.body;
   const updateProduct = async (id, newProduct) => { updateProduct = await productManager.updateProduct(id, newProduct) }
   updateProduct(id, newProduct)
 })
-
+// {
+//   "title": "POSTMAN",
+//   "description": "POSTMAN",
+//   "price": "POSTMAN",
+//   "thumbnail": "POSTMAN",
+//   "stock": "POSTMAN",
+//   "category": "POSTMAN",
+//   "status": true
+// }
+router.delete(`/:pid`, async(req, res)=>{
+  const id = (parseInt(req.params.pid))
+  console.log(id)
+  productManager.deleteProduct(id)
+})
 export default router;
